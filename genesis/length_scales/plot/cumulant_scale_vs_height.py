86e020def70b6e5dd5dfc1c4c7fd8af67be0b317
Leif Denby <leif@denby.eu>
2018-02-09 19:54:10 +0000

    utility to process and plot cumulant length-scales

---
+++ genesis/length_scales/plot/cumulant_scale_vs_height.py
@@ -0,0 +1,159 @@
+"""
+Routines for plotting cumulant characteristics from netCDF datafile
+"""
+import warnings
+import os
+
+import xarray as xr
+from matplotlib.gridspec import GridSpec
+
+
+FULL_SUITE_PLOT_PARTS = dict(
+    l=(0,0),
+    q=(0,slice(1,None)),
+    t=(1,slice(0,None)),
+    w=(2,slice(0,2)),
+    q_flux=(2,2),
+    t_flux=(2,3),
+    l_flux=(2,4),
+)
+
+
+def fix_cumulant_name(name):
+    name_mapping = {
+        'q': 'q_t',
+        't': r"\theta_l",
+        'l': 'q_l',
+        'q_flux': r"\overline{w'q_t'}",
+        't_flux': r"\overline{w'\theta_l}",
+        'l_flux': r"\overline{w'q_l'}",
+    }
+
+    v1, v2 = name.replace("C(", "").replace(")","").split(",")
+
+    v1_latex = name_mapping.get(v1, v1)
+    v2_latex = name_mapping.get(v2, v2)
+
+    return r"$C({},{})$".format(v1_latex, v2_latex)
+
+
+def plot_full_suite(data, marker=''):
+    gs = GridSpec(3, 5)
+    figure = plot.figure(figsize=(10, 12))
+
+    aspect = 0.3
+
+
+    ax = None
+    for var_name, s in FULL_SUITE_PLOT_PARTS.items():
+        ax = plot.subplot(gs[s], sharey=ax, adjustable='box-forced')
+        lines = []
+
+        d_ = data.sel(cumulant='C(l,l)', drop=True)
+        z_cb = d_.where(d_.width_principle>0.1, drop=True).zt.min()
+        ax.axhline(z_cb, linestyle=':', color='grey', alpha=0.6)
+
+        for p in data.param_name.values:
+
+            cumulant = "C({},{})".format(var_name, var_name)
+            d = data.sel(param_name=p, drop=True).sel(cumulant=cumulant, drop=True)
+
+            line, = plot.plot(d.width_principle, d.zt, marker=marker, label=str(p))
+            line2, = plot.plot(d.width_perpendicular, d.zt, marker=marker, label=str(p),
+                               linestyle='--', color=line.get_color())
+
+            plot.title(fix_cumulant_name(cumulant))
+
+            lines.append(line)
+            lines.append(line2)
+
+            plot.xlabel("characterisc width [m]")
+        
+        if s[1] == 0 or type(s[1]) == slice and s[1].start == 0:
+            plot.ylabel('height [m]')
+        else:
+            plot.setp(ax.get_yticklabels(), visible=False)
+
+    plot.tight_layout()
+
+    for ax in figure.axes:
+        # once the plots have been rendered we want to resize the xaxis so 
+        # that the aspect ratio is the same, note sharey on the subplot above
+        _, _, w, h = ax.get_position().bounds
+        ax.set_xlim(0, d.zt.max()/h*w*aspect)    
+
+    plot.subplots_adjust(bottom=0.10)
+    lgd = plot.figlegend(lines, [l.get_label() for l in lines], loc='lower center', ncol=2)
+
+
+def plot_default(data, marker=''):
+    plot.figure(figsize=(2.5*len(data.cumulant), 4))
+
+    z_ = data.zt
+    ax = None
+    for n in range(len(data.cumulant)):
+        cumulant = data.cumulant[n]
+        s = data.isel(cumulant=n, drop=True).squeeze()
+        ax = plot.subplot(1,len(data.cumulant),n+1, sharey=ax)
+        lines = []
+        for p in data.param_name.values:
+            d = data.sel(param_name=p, drop=True).sel(cumulant=cumulant, drop=True)
+
+            line, = plot.plot(d.width_principle, d.zt, marker='.', label=str(p))
+            line2, = plot.plot(d.width_perpendicular, d.zt, marker='.', label=str(p),
+                               linestyle='--', color=line.get_color())
+
+            lines.append(line)
+            lines.append(line2)
+
+        plot.title(data.cumulant.values[n])
+        plot.tight_layout()
+        
+        if n == 0:
+            plot.ylabel('height [m]')
+        else:
+            plot.setp(ax.get_yticklabels(), visible=False)
+
+    plot.subplots_adjust(bottom=0.24)
+    lgd = plot.figlegend(lines, [l.get_label() for l in lines], loc='lower center', ncol=2)
+
+
+if __name__ == "__main__":
+    import matplotlib
+    matplotlib.use("Agg")
+    import matplotlib.pyplot as plot
+
+    import seaborn as sns
+    sns.set(style='ticks')
+
+    import argparse
+    argparser = argparse.ArgumentParser(__doc__)
+    argparser.add_argument('input', help='input netCDF file')
+
+    args = argparser.parse_args()
+
+    dataset = xr.open_dataset(args.input)
+
+    full_suite_cumulants = [
+        "C({},{})".format(v,v) for v in FULL_SUITE_PLOT_PARTS.keys()
+    ]
+
+    missing_cumulants = [
+        c for c in full_suite_cumulants if not c in dataset.cumulant.values
+    ]
+
+    if not len(missing_cumulants) == 0:
+        warnings.warn("Not all variables for full suite plot, missing: {}"
+                      "".format(", ".join(missing_cumulants)))
+
+        plot_default(dataset)
+    else:
+        plot_full_suite(dataset)
+
+    sns.despine()
+
+    fn = os.path.basename(__file__).replace('.pyc', '.pdf')\
+                                   .replace('.py', '.pdf')
+    plot.savefig(fn, bbox_inches='tight')
+
+    print("Saved figure to {}".format(fn))
---
+++ genesis/length_scales/process/cumulant_scale_vs_height.py
@@ -0,0 +1,253 @@
+"""
+Utility script to process all 3D dataset and calculate cumulant length-scales
+in 2D domain-wide cross-sections
+"""
+import os
+
+import xarray as xr
+import numpy as np
+from tqdm import tqdm
+
+from collections import OrderedDict
+
+from genesis.length_scales import cumulant_analysis
+
+
+DATA_ROOT = os.environ.get(
+    'DATA_ROOT','/nfs/see-fs-02_users/earlcd/datastore/a289/LES_datasets/'
+)
+
+def z_center_field(phi_da):
+    assert phi_da.dims[-1] == 'zm'
+
+    # average vertical velocity to cell centers
+    zt_vals = 0.5*(phi_da.zm[1:].values + phi_da.zm[:-1].values)
+    zt = xr.DataArray(zt_vals, coords=dict(zt=zt_vals),
+                      attrs=dict(units='m'),dims=('zt',))
+
+    # create new coordinates for cell-centered vertical velocity
+    coords=OrderedDict(phi_da.coords)
+    del(coords['zm'])
+    coords['zt'] = zt
+
+    phi_cc_vals = 0.5*(phi_da[...,1:].values + phi_da[...,:-1].values)
+
+    dims = list(phi_da.dims)
+    dims[dims.index('zm')] = 'zt'
+
+    phi_cc = xr.DataArray(
+        phi_cc_vals, coords=coords, dims=dims,
+        attrs=dict(units=phi_da.units, longname=phi_da.longname)
+    )
+
+    phi_cc.name = phi_da.name
+
+    return phi_cc
+
+def compute_vertical_flux(phi_da, w_da):
+    """
+    Compute vertical flux of `phi_da` using vertical velocities `w_da`. Both are
+    expected to be xarray.DataArray
+    """
+    assert phi_da.time == w_da.time
+    assert phi_da.dims[-1] == 'zt'
+    assert w_da.dims[-1] == 'zm'
+
+    w_cc = z_center_field(phi_da=w_da)
+
+    w_horz_mean = w_cc.mean('x').mean('y')
+    v_horz_mean = phi_da.mean('x').mean('y')
+    dw = w_cc - w_horz_mean
+    dv = phi_da - v_horz_mean
+
+    v_flux = dw*dv
+    v_flux.attrs['units'] = "{} {}".format(w_da.units, phi_da.units)
+    v_flux.attrs['longname'] = "{} vertical flux".format(phi_da.longname)
+
+    return v_flux
+
+def get_height_variation_of_characteristic_scales(v1_3d, v2_3d, z_max, z_min=0.0):
+    datasets = []
+
+    z_ = v1_3d.zt[np.logical_and(v1_3d.zt > z_min, v1_3d.zt <= z_max)]
+
+    for z in tqdm(z_):
+        v1 = v1_3d.isel(time=0, drop=True)\
+                  .where(v1_3d.zt==z, drop=True).squeeze()
+
+        v2 = v2_3d.isel(time=0, drop=True)\
+                  .where(v2_3d.zt==z, drop=True).squeeze()
+
+        v1 = v1.transpose('x', 'y')
+        v2 = v2.transpose('x', 'y')
+
+        # copy over the shorthand name so that they can be used when naming the
+        # cumulant
+        v1.name = v1_3d.name
+        v2.name = v2_3d.name
+
+
+        scales = cumulant_analysis.charactistic_scales(v1=v1, v2=v2)
+
+        datasets.append(scales)
+
+    d = xr.concat(datasets, dim='zt')
+
+    # since these were all computations for the same cumulant we can set just
+    # one value which will be used for all. This will allow merge across
+    # cumulants later
+    d['cumulant'] = d.cumulant.values[0]
+
+    return d
+
+
+def process(model_name, case_name, param_names, variable_sets, z_min, z_max, tn):
+    datasets = []
+
+    for param_name in param_names:
+        param_datasets = []
+
+        for var_name_1, var_name_2 in variable_sets:
+            v1_3d = get_data(model_name, case_name, param_name,
+                             var_name=var_name_1, tn=tn)
+
+            if var_name_2 != var_name_2:
+                v2_3d = get_data(model_name, case_name, param_name,
+                                 var_name=var_name_2, tn=tn)
+            else:
+                v2_3d = v1_3d
+
+
+            characteristic_scales = get_height_variation_of_characteristic_scales(
+                v1_3d=v1_3d, v2_3d=v2_3d, z_max=z_max, z_min=z_min,
+            )
+
+            characteristic_scales['dataset'] = "{}__{}".format(case_name, param_name)
+            param_datasets.append(characteristic_scales)
+
+        dataset_param = xr.concat(param_datasets, dim='cumulant')
+        # all cumulant calculations were for the same dataset, set one value
+        # so we can concat later across `dataset`
+        dataset_param['dataset'] = dataset_param.dataset.values[0]
+
+        datasets.append(dataset_param)
+
+    all_data = xr.concat(datasets, dim='dataset')
+
+
+    return all_data
+
+
+def get_fn(model_name, case_name, param_name, var_name, tn):
+    return os.path.join(DATA_ROOT, model_name, case_name, param_name, 
+                        '{}.tn{}.{}.nc'.format(case_name, tn, var_name))
+
+def get_data(model_name, case_name, param_name, var_name, tn):
+    compute_flux = False
+    if var_name.endswith('_flux'):
+        var_name = var_name.replace('_flux', '')
+        compute_flux = True
+
+    fn = get_fn(model_name, case_name, param_name, var_name, tn)
+    try:
+        phi_da = xr.open_dataarray(fn, decode_times=False)
+        phi_da = phi_da.rename(dict(xt='x', yt='y'))
+        phi_da.name = var_name
+    except IOError:
+        print fn
+        raise
+
+
+    if var_name == 'w':
+        phi_da = z_center_field(phi_da=phi_da)
+
+
+    if not compute_flux:
+        return phi_da
+    else:
+        fn_w = get_fn(model_name, case_name, param_name, 'w', tn)
+        w_da = xr.open_dataarray(fn_w, decode_times=False)
+        w_da = w_da.rename(dict(xt='x', yt='y'))
+
+        phi_flux_da = compute_vertical_flux(phi_da=phi_da, w_da=w_da)
+        phi_flux_da.name = "{}_flux".format(var_name)
+
+        return phi_flux_da
+
+
+def get_cross_section(model_name, case_name, var_name, z, tn,
+                      param_name, z_max=700., method=None):
+
+    ar = get_data(model_name, case_name, param_name, var_name=var_name, tn=tn)
+
+    da = ar.sel(zt=z, drop=True, method=method).squeeze()
+
+    da = da.transpose('x', 'y')
+
+    da['zt'] = ((), z, dict(units='m'))
+
+    return da
+
+
+def run_default():
+    model = 'uclales'
+    case_name = 'rico'
+
+    VARIABLE_SETS = (
+        ('w', 'w'),
+        ('q', 'q'),
+        ('t', 't'),
+        ('l', 'l'),
+        ('q_flux', 'q_flux'),
+        ('t_flux', 't_flux'),
+        ('l_flux', 'l_flux'),
+    )
+
+    PARAM_NAMES = [
+        'fixed_flux_shear/nx800',
+        'fixed_flux_noshear/nx800',
+    ]
+
+    return process(model_name, case_name, PARAM_NAMES, VARIABLE_SETS,
+                   z_min=0., z_max=700., tn=3)
+
+if __name__ == "__main__":
+    import argparse
+
+    argparser = argparse.ArgumentParser(__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    DEFAULT_VARS = "w q t l q_flux t_flux l_flux".split(" ")
+
+    argparser.add_argument('case_name', help='e.g. `rico`', type=str)
+    argparser.add_argument('--param_name',
+        help='e.g. `fixed_flux_shear/nx800`', type=str, default='',
+        nargs='+')
+    argparser.add_argument('tn', type=int)
+    argparser.add_argument('--vars', default=DEFAULT_VARS, nargs="+")
+    argparser.add_argument('--model', default='uclales')
+    argparser.add_argument('--z_max', default=700., type=float)
+    argparser.add_argument('--z_min', default=0., type=float)
+
+    args = argparser.parse_args()
+
+    if args.model != 'uclales':
+        raise NotImplementedError
+
+    variable_sets = zip(args.vars, args.vars)
+
+    data = process(
+        model_name=args.model,
+        case_name=args.case_name,
+        param_names=args.param_name,
+        variable_sets=variable_sets,
+        z_min=args.z_min,
+        z_max=args.z_max,
+        tn=args.tn
+    )
+
+    fn = os.path.basename(__file__).replace('.pyc', '.nc')\
+                                   .replace('.py', '.nc')
+    data.to_netcdf(fn)
+    print("Output written to {}".format(fn))
