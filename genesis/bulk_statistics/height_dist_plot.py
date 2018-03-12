07557000e2e415a9a842e619917bdd6de5b71f51
Leif Denby <leif@denby.eu>
2018-03-12 16:45:15 +0000

    Bulk statistics for large files

--- genesis/bulk_statistics/height_dist_plot.py
+++ genesis/bulk_statistics/height_dist_plot.py
@@ -9,6 +9,9 @@ UCLALES:
 - q: total water vapour
 - t: liquid potential temperature
 """
+import os
+import matplotlib
+matplotlib.use("Agg")
 
 import xarray as xr
 import matplotlib.pyplot as plot
@@ -16,21 +19,20 @@ import numpy as np
 
 
 def height_dist_plot(dataset, var_name, t, dz, scaling=25.*4, z_max=700.,
-                     dvar=0.05, mask_zero=False):
-    calc_flux = var_name.endswith('flux')
-    if calc_flux:
-        var_name = var_name.replace('_flux', '')
+                     dvar=0.05, mask_zero=False, cumulative=False, marker='.'):
 
-    z_var = dataset[var_name].coords.values()[1]
+    z_var = dataset[var_name].coords[
+        'zt' if 'zt' in dataset[var_name].coords else 'zm'
+    ]
 
     def calc_bins(v_, dv_):
         try:
-            v_min = dv_*int(v_.min()/dv_)
-            v_max = dv_*int(v_.max()/dv_)
+            v_min = dv_*np.floor(v_.min()/dv_)
+            v_max = dv_*np.ceil(v_.max()/dv_)
         except ValueError:
             return None, 1
 
-        n = int((v_max - v_min)/dv_)
+        n = int(np.round((v_max - v_min)/dv_))
 
         if n == 0:
             return None, 1
@@ -47,20 +49,6 @@ def height_dist_plot(dataset, var_name, t, dz, scaling=25.*4, z_max=700.,
 
         units = dataset_[var_name].units
 
-        if calc_flux:
-            z_up = float(z_) + 0.5*dz
-            w_up = get_zdata('zm', z_up)['w']
-            z_down = float(z_) - 0.5*dz
-            if np.any(z_down > 0.0):
-                w_down = get_zdata('zm', z_down)['w']
-            else:
-                w_down = np.zeros_like(d)
-            w_c = 0.5*(w_up + w_down)
-
-            d = np.array(w_c*(d - d.mean()))
-
-            units += ' m/s'
-
         if mask_zero:
             d = d[~(d == 0)]
 
@@ -68,9 +56,13 @@ def height_dist_plot(dataset, var_name, t, dz, scaling=25.*4, z_max=700.,
         bin_counts, bin_edges = np.histogram(
             d, normed=True, bins=n_bins, range=bin_range
         )
+
+        if cumulative:
+            bin_counts = np.cumsum(bin_counts[::-1])[::-1]
+
         bin_centers = 0.5*(bin_edges[1:] + bin_edges[:-1])
         s = scaling/bin_counts.max()
-        plot.plot(bin_centers, bin_counts*s+np.array(z_), marker='.',)
+        plot.plot(bin_centers, bin_counts*s+np.array(z_), marker=marker,)
         plot.axhline(z_, linestyle=':', color='grey', alpha=0.3)
 
         # print float(z_), d.min(), d.max(), dataset_[var_name].shape
@@ -81,9 +73,10 @@ def height_dist_plot(dataset, var_name, t, dz, scaling=25.*4, z_max=700.,
     if d.min() < 0.0 < d.max():
         plot.axvline(0.0, linestyle=':', color='grey')
 
-    if calc_flux:
-        var_label = var_name
-        if var_name == 't':
+    is_flux = var_name.endswith('flux')
+    if is_flux:
+        var_label = var_name.replace('_flux', '')
+        if var_label == 't':
             var_label = '\\theta_l'
 
         plot.xlabel(r"$\overline{w'%s'}$ [%s]" % (var_label, units))
@@ -96,28 +89,52 @@ def height_dist_plot(dataset, var_name, t, dz, scaling=25.*4, z_max=700.,
 
 
 def find_dz(dataset):
-    dz_ = np.diff(dataset.zt)
+    z_var = 'zt' if 'zt' in dataset[var_name].coords else 'zm'
+    dz_ = np.diff(dataset[z_var])
 
     assert np.min(dz_) == np.max(dz_)
 
     return np.min(dz_)
+
+def _add_flux_var(dataset, var_name):
+    z_up = float(z_) + 0.5*dz
+    w_up = get_zdata('zm', z_up)['w']
+    z_down = float(z_) - 0.5*dz
+    if np.any(z_down > 0.0):
+        w_down = get_zdata('zm', z_down)['w']
+    else:
+        w_down = np.zeros_like(d)
+    w_c = 0.5*(w_up + w_down)
+
+    d = dataset[var_name].values
+    d = np.array(w_c*(d - d.mean()))
+
+    units = '{} m/s'.format(dataset[var_name].units)
+
+    dataset['{}_flux'.format(var_name)] = (
+        dataset[var_name].dims,
+        d,
+        dict(units=units)
+    )
+
+    return dataset
 
 
 if __name__ == "__main__":
     import argparse
     argparser = argparse.ArgumentParser(__doc__)
 
-    argparser.add_argument('input.nc')
+    argparser.add_argument('input')
     default_vars = "q t w q_flux t_flux".split(" ")
     argparser.add_argument('--vars', nargs="+", default=default_vars)
-    argparser.add_argument('--time', nargs="+", type=float, required=True)
+    argparser.add_argument('--time', nargs="+", type=float, required=False)
     argparser.add_argument('--z_max', type=float, default=650.)
+    argparser.add_argument('--cumulative', default=False, action="store_true")
+    argparser.add_argument('--bin-marker', default='', type=str)
 
     args = argparser.parse_args()
 
     num_vars = len(args.vars)
-    num_timesteps = len(args.time)
-    plot.figure(figsize=(5*num_timesteps,3*num_vars))
 
     default_scalings = dict(
         q=400.,
@@ -131,21 +148,74 @@ if __name__ == "__main__":
         q_flux=4.0e-5
     )
 
-    dataset = xr.open_dataset(vars(args)['input.nc'], decode_times=False)
+    input_name = args.input
+
+    if input_name.endswith('.nc'):
+        dataset = xr.open_dataset(input_name, decode_times=False)
+
+        for var_name in enumerate(args.vars):
+            if var_name.endswith('_flux') and not var_name in dataset:
+                _add_flux_var(dataset, var_name.replace('_flux', ''))
+
+        out_filename = input_name.replace('.nc', '.bl_hist_plots.pdf')
+    else:
+        out_filename = input_name + '.bl_hist_plots.pdf'
+
+        # have to handle `w` seperately because it is staggered
+        filenames = [
+            "{}.{}.nc".format(input_name, var_name) for var_name in args.vars
+            if not var_name == 'w'
+        ]
+        missing = [
+            fn for fn in filenames if not os.path.exists(fn)
+        ]
+
+        if len(filenames) > 0:
+            if len(missing) > 0:
+                raise Exception("Missing files: {}".format(", ".join(missing)))
+
+            dataset = xr.open_mfdataset(filenames, decode_times=False,
+                                        concat_dim=None, chunks=dict(zt=20))
+        else:
+            dataset = None
+
+        if 'w' in args.vars:
+            d2 = xr.open_dataset("{}.w.nc".format(input_name),
+                                 decode_times=False, chunks=dict(zm=20))
+
+            if not dataset is None:
+                dataset = xr.merge([dataset, d2])
+            else:
+                dataset = d2
 
     dz = find_dz(dataset)
 
+    if args.time is None:
+        num_timesteps = 1
+        times = dataset.time.values
+    else:
+        num_timesteps = len(args.time)
+        times = args.time
+
+    plot.figure(figsize=(5*num_timesteps,3*num_vars))
+
     for n, var_name in enumerate(args.vars):
-        for m, t in enumerate(args.time):
+        for m, t in enumerate(times):
             plot.subplot(num_vars, num_timesteps, n*num_timesteps + m+1)
             scaling = default_scalings.get(var_name, 100.)
             binsize = default_binsize.get(var_name, 0.1)
             height_dist_plot(dataset, var_name, dz=dz, scaling=scaling,
-                             t=t, dvar=binsize, z_max=args.z_max)
+                             t=t, dvar=binsize, z_max=args.z_max,
+                             cumulative=args.cumulative,
+                             marker=args.bin_marker)
 
-    out_filename = 'bl_hist_plots.pdf'
 
     plot.tight_layout()
+
+    plot.suptitle("{}distribution in {}".format(
+        ["", "Cumulative "][args.cumulative],
+        input_name
+    ))
 
     plot.savefig(out_filename)
 
