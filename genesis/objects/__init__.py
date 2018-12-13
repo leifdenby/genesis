37331f5337110c957e0b64bf1aee5adde6af790f
Leif Denby <leif@denby.eu>
2018-12-13 18:20:18 +0000

    fix for minkowski scales datasets loading

--- genesis/objects/__init__.py
+++ genesis/objects/__init__.py
@@ -4,8 +4,9 @@ import os
 import xarray as xr
 
 def get_data(base_name, mask_identifier='*', debug=False):
+    print(base_name, mask_identifier)
     glob_patterns = [
-        "{}.minkowski_scales.{}.nc".format(base_name, mask_identifier),
+        "{}.objects.{}.minkowski_scales.nc".format(base_name, mask_identifier),
         "{}.objects.{}.integral.*.nc".format(base_name, mask_identifier)
     ]
 
@@ -21,16 +22,15 @@ def get_data(base_name, mask_identifier='*', debug=False):
     ds = xr.open_mfdataset(fns)
 
     # 4/3*pi*r**3 => r = (3/4*1/pi*v)**(1./3.)
-    ds['r_equiv'] = (3./(4.*3.14)*ds.volume__sum)**(1./3.)
-    ds.r_equiv.attrs['units'] = 'm'
-    ds.r_equiv.attrs['longname'] = 'equivalent radius'
+    if 'volume__sum' in ds.data_vars:
+        ds['r_equiv'] = (3./(4.*3.14)*ds.volume__sum)**(1./3.)
+        ds.r_equiv.attrs['units'] = 'm'
+        ds.r_equiv.attrs['longname'] = 'equivalent radius'
 
     return ds
 
 def make_mask_from_objects_file(filename):
     object_file = filename.replace('.nc', '')
-
-    print(object_file)
 
     if not 'objects' in object_file:
         raise Exception()
