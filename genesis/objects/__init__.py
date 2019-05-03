c710bc6dadb1d78923bf6b7696e247c0c8f8e67b
Leif Denby <leif@denby.eu>
2019-05-03 14:51:48 +0100

    minor fix for py3

--- genesis/objects/__init__.py
+++ genesis/objects/__init__.py
@@ -3,6 +3,12 @@ import os
 
 import xarray as xr
 
+try:
+    # python3 has moved reduce to functools
+    from functools import reduce
+except ImportError:
+    pass
+
 def get_data(base_name, mask_identifier='*', debug=False):
     print(base_name, mask_identifier)
     glob_patterns = [
