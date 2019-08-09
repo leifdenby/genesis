6407c9e58c4edccda85fcee065ff85ad8e5a7795
Leif Denby <leif@denby.eu>
2019-08-09 09:09:09 +0100

    generalise cell-centering code

--- genesis/utils/calc_flux.py
+++ genesis/utils/calc_flux.py
@@ -25,23 +25,36 @@ def get_horz_devition(var_name):
 
     return xr.open_dataarray(fn_dv, decode_times=False, chunks=xr_chunks)
 
-def z_center_field(phi_da):
-    assert phi_da.dims[-1] == 'zm'
+
+def center_staggered_field(phi_da):
+    """
+    Create cell-centered values for staggered (velocity) fields
+    """
+    dim = [d for d in phi_da.dims if d.endswith('m')][0]
+    newdim = dim.replace('m', 't')
+
+    s_left, s_right = slice(0, -1), slice(1, None)
 
     # average vertical velocity to cell centers
-    zt_vals = 0.5*(phi_da.zm[1:].values + phi_da.zm[:-1].values)
-    zt = xr.DataArray(zt_vals, coords=dict(zt=zt_vals),
-                      attrs=dict(units='m'),dims=('zt',))
+    coord_vals = 0.5*(
+        phi_da[dim].isel(**{dim:s_left}).values
+      + phi_da[dim].isel(**{dim:s_right}).values
+    )
+    coord = xr.DataArray(coord_vals, coords={newdim: coord_vals},
+                      attrs=dict(units='m'),dims=(newdim,))
 
     # create new coordinates for cell-centered vertical velocity
     coords=OrderedDict(phi_da.coords)
-    del(coords['zm'])
-    coords['zt'] = zt
+    del(coords[dim])
+    coords[newdim] = coord
 
-    phi_cc_vals = 0.5*(phi_da[...,1:].values + phi_da[...,:-1].values)
+    phi_cc_vals = 0.5*(
+        phi_da.isel(**{dim:s_left}).values
+      + phi_da.isel(**{dim:s_right}).values
+    )
 
     dims = list(phi_da.dims)
-    dims[dims.index('zm')] = 'zt'
+    dims[dims.index(dim)] = newdim
 
     phi_cc = xr.DataArray(
         phi_cc_vals, coords=coords, dims=dims,
@@ -51,6 +64,9 @@ def z_center_field(phi_da):
     phi_cc.name = phi_da.name
 
     return phi_cc
+
+def z_center_field(phi_da):
+    return center_staggered_field(phi_da)
 
 def get_cell_centered_vertical_velocity():
 
