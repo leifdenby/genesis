0418528a3de3b43b79257c05fdacf733f68c1188
Leif Denby <leif@denby.eu>
2019-09-19 13:15:13 +0100

    refactor cumulant extraction in pipeline

--- genesis/utils/pipeline/viz.py
+++ genesis/utils/pipeline/viz.py
@@ -20,59 +20,6 @@ from .. import plot_types
 
 from . import data
 
-class ExtractCumulantScaleProfile(luigi.Task):
-    base_name = luigi.Parameter()
-    v1 = luigi.Parameter()
-    v2 = luigi.Parameter()
-    z_max = luigi.FloatParameter(default=700.)
-    mask = luigi.Parameter(default=None)
-    mask_args = luigi.Parameter(default='')
-    width_method = length_scales.cumulant.calc.WidthEstimationMethod.MASS_WEIGHTED
-
-    def requires(self):
-        reqs = {}
-        reqs['fields'] = [
-                data.ExtractField3D(base_name=self.base_name,
-                                    field_name=self.v1),
-                data.ExtractField3D(base_name=self.base_name,
-                                    field_name=self.v2),
-        ]
-
-        if self.mask is not None:
-            reqs['mask'] = data.MakeMask(method_name=self.mask,
-                                         method_extra_args=self.mask_args,
-                                         base_name=self.base_name
-                                         )
-
-        return reqs
-
-    def run(self):
-        da_v1 = self.input()['fields'][0].open(decode_times=False)
-        da_v2 = self.input()['fields'][1].open(decode_times=False)
-
-        calc_fn = length_scales.cumulant.vertical_profile.calc.get_height_variation_of_characteristic_scales
-
-        mask = None
-        if self.mask:
-            mask = self.input()['mask'].open(decode_times=False)
-
-
-        import ipdb
-        with ipdb.launch_ipdb_on_exception():
-            da = calc_fn(
-                v1_3d=da_v1, v2_3d=da_v2, width_method=self.width_method,
-                z_max=self.z_max, mask=mask
-            )
-
-        da.to_netcdf(self.output().path)
-
-    def output(self):
-        fn = length_scales.cumulant.vertical_profile.calc.FN_FORMAT.format(
-            base_name=self.base_name, v1=self.v1, v2=self.v2,
-            mask=self.mask or "no_mask"
-        )
-        return data.XArrayTarget(fn)
-
 class CumulantScalesProfile(luigi.Task):
     base_names = luigi.Parameter()
     cumulants = luigi.Parameter()
@@ -92,7 +39,7 @@ class CumulantScalesProfile(luigi.Task):
 
         for base_name in self.base_names.split(','):
             reqs[base_name] = [
-                ExtractCumulantScaleProfile(
+                data.ExtractCumulantScaleProfile(
                     base_name=base_name, v1=c[0], v2=c[1],
                     mask=self.mask, mask_args=self.mask_args,
                 )
