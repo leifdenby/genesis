1c5679b5879e449dce5e083525898b25413b1cfd
Leif Denby <leif@denby.eu>
2019-11-12 12:54:29 +0000

    add more output to pipeline plot suite

--- genesis/utils/pipeline/viz.py
+++ genesis/utils/pipeline/viz.py
@@ -460,7 +460,7 @@ class CrossSection(luigi.Task):
         da.coords['xt'].attrs['long_name'] = 'horz. dist.'
         da.coords['yt'].attrs['long_name'] = 'horz. dist.'
 
-        z = [float(v) for v in self.z.split(',')]
+        z = sorted([float(v) for v in self.z.split(',')], reverse=True)
         da_sliced = da.sel(zt=z, method='nearest')
         da_sliced.attrs.update(da_[0].attrs)
 
@@ -1221,16 +1221,41 @@ class Suite(luigi.Task):
 
     DISTS = dict(
         mean_profile='mean.profiles',
+        cross_section='cross.sections',
+        cumulant_profiles='cumulant.profiles',
     )
+
+    CROSS_SECTION_VARS = ['qv', 'w', 'qc', 'cvrxp_p_stddivs']
 
     def requires(self):
         reqs = {}
+        if self.timestep is not None:
+            add_timestep = lambda bn: "{}.tn{}".format(bn, self.timestep)
+        else:
+            add_timestep = lambda bn: bn
+
         if self.base_name is None:
             datasources = data.get_datasources()
             for base_name in datasources.keys():
-                if self.timestep is not None:
-                    base_name = "{}.tn{}".format(base_name, self.timestep)
-                reqs[base_name] = Suite(base_name=base_name)
+                reqs['subsuite__{}'.format(base_name)] = Suite(
+                    base_name=add_timestep(base_name)
+                )
+
+            for v in Suite.CROSS_SECTION_VARS:
+                base_names = ",".join([
+                    add_timestep(bn) for bn in datasources.keys()
+                ])
+                reqs['cross_section__{}'.format(v)] = CrossSection(
+                    base_names=base_names,
+                    var_name=v,
+                    z="100.,400.,600.,800."
+                )
+                reqs['cumulant_profiles__{}'.format(v)] = CumulantScalesProfile(
+                    base_names=base_names,
+                    cumulants="w:w,qv:qv,qc:qc,cvrxp:cvrxp,w:qv,w:qc,w:cvrxp",
+                    var_name=v,
+                    z_max=1000.,
+                )
         else:
             reqs['mean_profile'] = HorizontalMeanProfile(
                 base_name=self.base_name,
@@ -1238,21 +1263,28 @@ class Suite(luigi.Task):
         return reqs
 
     def run(self):
-        if self.base_name is not None:
-            for comp, target in self.input().items():
-                dst_path = self._build_output_path(comp=comp, target=target)
-                dst_path.parent.mkdir(parents=True, exist_ok=True)
-                shutil.move(target.fn, dst_path)
+        for comp, target in self.input().items():
+            if comp.startswith('subsuite__'):
+                continue
+            dst_path = self._build_output_path(comp=comp, target=target)
+            dst_path.parent.mkdir(parents=True, exist_ok=True)
+            shutil.move(target.fn, dst_path)
 
     def _build_output_path(self, comp, target):
-        d = self.DISTS[comp]
+        d = self.DISTS[comp.split('__')[0]]
         return Path(d)/target.fn
 
     def output(self):
-        if self.base_name is None:
-            return self.input()
+        outputs = []
+        for (comp, target) in self.input().items():
+            if comp.startswith('subsuite__'):
+                outputs.append(target)
+            else:
+                p = self._build_output_path(comp=comp, target=target)
+                moved_target = luigi.LocalTarget(p)
+                outputs.append(moved_target)
+
+        if len(outputs) == 1:
+            return outputs[0]
         else:
-            return [
-                self._build_output_path(comp, target)
-                for (comp, target) in self.input().items()
-            ]
+            return outputs
