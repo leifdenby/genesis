ed99116a7c71263079ad0d2239776b4c191ce892
Leif Denby <leif@denby.eu>
2019-12-17 13:45:23 +0000

    move center_staggered_field to utils

--- genesis/utils/__init__.py
+++ genesis/utils/__init__.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.constants import pi
+import xarray as xr
 
+from collections import OrderedDict
 import warnings
 
 REQUIRED_DX_PRECISION = 4
@@ -70,3 +72,42 @@ def wrap_angles(theta):
             return v
 
     return _wrap(theta)
+
+def center_staggered_field(phi_da):
+    """
+    Create cell-centered values for staggered (velocity) fields
+    """
+    dim = [d for d in phi_da.dims if d.endswith('m')][0]
+    newdim = dim.replace('m', 't')
+
+    s_left, s_right = slice(0, -1), slice(1, None)
+
+    # average vertical velocity to cell centers
+    coord_vals = 0.5*(
+        phi_da[dim].isel(**{dim:s_left}).values
+      + phi_da[dim].isel(**{dim:s_right}).values
+    )
+    coord = xr.DataArray(coord_vals, coords={newdim: coord_vals},
+                      attrs=dict(units='m'),dims=(newdim,))
+
+    # create new coordinates for cell-centered vertical velocity
+    coords=OrderedDict(phi_da.coords)
+    del(coords[dim])
+    coords[newdim] = coord
+
+    phi_cc_vals = 0.5*(
+        phi_da.isel(**{dim:s_left}).values
+      + phi_da.isel(**{dim:s_right}).values
+    )
+
+    dims = list(phi_da.dims)
+    dims[dims.index(dim)] = newdim
+
+    phi_cc = xr.DataArray(
+        phi_cc_vals, coords=coords, dims=dims,
+        attrs=dict(units=phi_da.units, long_name=phi_da.long_name)
+    )
+
+    phi_cc.name = phi_da.name
+
+    return phi_cc
