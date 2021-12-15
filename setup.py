15d45e299f4c0992be93acc385e04440abb3540b
Leif Denby <leif@denby.eu>
2021-12-15 14:15:47 +0000

    Move to setup.cfg

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-from setuptools import setup, find_packages
+import setuptools
 
-# note: version is maintained inside convml_tt/version.py
-exec(open("genesis/version.py").read())
-
-setup(
-    name="genesis",
-    packages=find_packages(exclude=["contrib", "tests", "docs"]),
-    version=__version__,  # noqa
-    description="GENESIS toolkit for analysing coherent structures in atmospheric flows",
-    author="Leif Denby",
-    author_email="leifdenby@gmail.com",
-    url="https://github.com/leifdenby/genesis",
-    classifiers=[],
-)
+setuptools.setup()
