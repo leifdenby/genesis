d97158df698568783c6f16e2bc9dd1382a57a1f1
Leif Denby <leif@denby.eu>
2022-08-23 09:11:42 +0100

    add missing exports

--- genesis/utils/pipeline/data_sources/uclales/__init__.py
+++ genesis/utils/pipeline/data_sources/uclales/__init__.py
@@ -1,7 +1,6 @@
 from . import tracking_2d  # noqa
-
-from .base import ( # noqa
+from .base import (  # noqa
+    DERIVED_FIELDS,
     build_runtime_cross_section_extraction_task,
     extract_field_to_filename,
-    DERIVED_FIELDS,
 )
